8866058562cfa028dd61f4913e2c2e56f3124161
NXDuyThang <xuanthanghhd@gmail.com>
2026-06-05 09:58:08 +0700

    Fix Vietnamese font rendering

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -16,18 +16,18 @@
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;300;400;600;700&family=Mrs+Saint+Delafield&family=Miniver&family=Open+Sans+Condensed:wght@300;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&family=Great+Vibes&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&family=Jost:wght@300;400;500;600;700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         body {
-            font-family: 'Josefin Sans', sans-serif;
+            font-family: 'Montserrat', sans-serif;
             background-color: #040810;
         }
         .font-script-tagline {
-            font-family: 'Mrs Saint Delafield', cursive;
+            font-family: 'Great Vibes', cursive;
         }
         /* Thin lines like Laurent theme layout */
         .grid-line-vertical {
